752b494b662cb078af67536be820bbd40be251a4
Overlander <anuragnandal@hotmail.com>
2022-04-19 10:13:00 -0400

    Add 'build_tree' method

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,4 +1,7 @@
 class Node
+  attr_reader :data
+  attr_accessor :left_child, :right_child
+
   def initialize(data = nil, left_child = nil, right_child = nil)
     @data = data
     @left_child = left_child
@@ -7,14 +10,21 @@ class Node
 end
 
 class Tree
-  attr_accessor :root
+  attr_reader :root
 
   def initialize(array)
     @array = array
-    build_tree(@array.sort!.uniq!)
+    @root = build_tree(@array.sort!.uniq!)
   end
 
-  def build_tree(array)
+  def build_tree(array, starting = 0, ending = array.length - 1)
+    return nil if starting > ending
+
+    mid = (starting + ending) / 2
+    root = Node.new(array[mid])
+    root.left_child = build_tree(array, starting, mid - 1)
+    root.right_child = build_tree(array, mid + 1, ending)
+    root
   end
 end
 
